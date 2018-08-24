05024cef42792d551117a016da365dcda224978a
dennisleprozo <dennisvleprozo@gmail.com>
2018-08-24 10:29:20 -0600

    initial

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+// import logo from './logo.svg';
+import './App.css';
+import Nav from './components/Nav/Nav';
+import Auth from './components/Auth/Auth';
+import Dashboard from './components/Dashboard/Dashboard';
+import Form from './components/Form/Form';
+import Post from './components/Post/Post';
+// import routes from './routes'
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Nav />
+        <Auth />
+        <Dashboard />
+        <Form />
+        <Post />
+        {/* {routes} */}
+      </div>
+    );
+  }
+}
+
+export default App;
